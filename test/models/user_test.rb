0e28e2855fa10409d27e149d752629bf896e5478
Jose Luis <Jose3L@users.noreply.github.com>
2014-10-23 14:00:22 +0200

    Added test for user
    For:
    -first_name
    -last_name
    -profile_name

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "a user should enter a first name" do
+		user = User.new
+		assert !user.save	#indica que el usuario no debe ser guardado en la BD
+		#comprueba si el nombre esta vacio
+		assert !user.errors[:first_name].empty?
+	end
+
+	test "a user should enter a last name" do
+		user = User.new
+		assert !user.save	#indica que el usuario no debe ser guardado en la BD
+		#comprueba si el nombre esta vacio
+		assert !user.errors[:last_name].empty?
+	end
+
+	test "a user should enter a profile name" do
+		user = User.new
+		assert !user.save	#indica que el usuario no debe ser guardado en la BD
+		#comprueba si el nombre esta vacio
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a unique profile names" do
+		user = User.new
+		user.profile_name = users(:jason).profile_name
+		
+		users(:jason)
+
+		assert !user.save	#indica que el usuario no debe ser guardado en la BD
+		puts user.errors.inspect
+		#comprueba si el nombre esta vacio
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a user should have a profile name without spaces" do
+		user = User.new
+		user.profile_name = "My profile with spaces"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
 end
